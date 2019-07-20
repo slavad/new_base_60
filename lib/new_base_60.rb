7a70880771195c2ae09b7c7d4dedc985c0ce7083
Viacheslav Dushin <slava@dushin.pro>
2019-07-20 17:19:52 +0300

    new methods

--- lib/new_base_60.rb
+++ lib/new_base_60.rb
@@ -4,72 +4,91 @@ require "time"
 class NewBase60
   VERSION    = '1.1.3'
   VOCABULARY = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_abcdefghijkmnopqrstuvwxyz"
+  PREFIX = "0:"
 
   def initialize(base_60)
     @base_60 = base_60
+    @to_i = to_i
   end
 
   def to_s
     if negative?
-      "-0:#{abs}"
+      "-#{PREFIX}#{string_abs}"
     else
-      "0:#{abs}"
+      "#{PREFIX}#{string_abs}"
     end
   end
 
   alias inspect to_s
 
+  def ==(other_sxg)
+    to_i == other_sxg.to_i
+  end
+
+  def negative?
+    to_i.negative?
+  end
+
+  def positive?
+    to_i.positive?
+  end
+
+  def zero?
+    to_i.zero?
+  end
+
+  # Converts into a base 10 integer.
+  def to_i
+    @to_i ||= lambda do
+      num = 0
+      coeff = 1
+      if @base_60.bytes.first == 45 # minus sign
+        coeff = -1
+      end
+
+      @base_60.bytes.each do |char|
+        case char
+        when 48..57   then char -= 48
+        when 65..72   then char -= 55
+        when 73, 108  then char  = 1  # typo capital I, lowercase l to 1
+        when 74..78   then char -= 56
+        when 79       then char  = 0  # error correct typo capital O to 0
+        when 80..90   then char -= 57
+        when 95       then char  = 34
+        when 97..107  then char -= 62
+        when 109..122 then char -= 63
+        else               char  = 0  # treat all other noise as 0
+        end
+
+        num = 60 * num + char
+      end
+
+      coeff*num
+    end.call
+  end
+
+  # Converts into a Date.
+  def to_date
+    @to_date ||= Time.at(to_i * 60 * 60 * 24).utc.to_date
+  end
+
   def abs
+    @abs ||= NewBase60.new(string_abs)
+  end
+
+  def string_abs
     if negative?
       @base_60[1..-1]
     else
       @base_60
     end
   end
-
-  def negative?
-    # minus sign
-    @base_60.bytes.first == 45
-  end
-
-  # Converts into a base 10 integer.
-  def to_i
-    num = 0
-    coeff = 1
-    if negative?
-      coeff = -1
-    end
-
-    @base_60.bytes.each do |char|
-      case char
-      when 48..57   then char -= 48
-      when 65..72   then char -= 55
-      when 73, 108  then char  = 1  # typo capital I, lowercase l to 1
-      when 74..78   then char -= 56
-      when 79       then char  = 0  # error correct typo capital O to 0
-      when 80..90   then char -= 57
-      when 95       then char  = 34
-      when 97..107  then char -= 62
-      when 109..122 then char -= 63
-      else               char  = 0  # treat all other noise as 0
-      end
-
-      num = 60 * num + char
-    end
-
-    coeff*num
-  end
-
-  # Converts into a Date.
-  def to_date
-    Time.at(to_i * 60 * 60 * 24).utc.to_date
-  end
 end
 
 class Integer
   # Converts a base 10 integer into a NewBase60 string.
   def to_sxg
-    return "0" if zero?
+    return "#{NewBase60::PREFIX}0" if zero?
 
     num = self.abs
     sxg = ""
@@ -88,11 +107,15 @@ class Integer
   # Converts a base 10 integer into a NewBase60 string,
   # padding with leading zeroes.
   def to_sxgf(padding=1)
-    str = to_sxg
+    str = to_sxg.string_abs
 
     padding -= str.length
 
-    padding.times.map { "0" }.join + str
+    result = padding.times.map { "0" }.join + str
+    if negative?
+      return "-#{NewBase60::PREFIX}#{result}"
+    end
+    "#{NewBase60::PREFIX}#{result}"
   end
 end
 
--- test/test_new_base_60.rb
+++ test/test_new_base_60.rb
@@ -3,6 +3,50 @@ require 'minitest/autorun'
 require 'new_base_60'
 
 class TestCase < MiniTest::Spec
+  def test_string_abs
+    assert NewBase60.new("464").string_abs == "464"
+    assert NewBase60.new("-464").string_abs == "464"
+  end
+
+  def test_abs
+    assert NewBase60.new("464").abs == NewBase60.new("464")
+    assert NewBase60.new("-464").abs == NewBase60.new("464")
+  end
+
+  def test_equals
+    assert NewBase60.new("464") == NewBase60.new("464")
+    assert NewBase60.new("0") == NewBase60.new("0")
+    assert NewBase60.new("-464") == NewBase60.new("-464")
+  end
+
+  def test_negative
+    refute NewBase60.new("464").negative?
+    refute NewBase60.new("0").negative?
+    assert NewBase60.new("-464").negative?
+  end
+
+  def test_positive
+    refute NewBase60.new("-464").positive?
+    refute NewBase60.new("0").positive?
+    assert NewBase60.new("464").positive?
+  end
+
+  def test_zero
+    refute NewBase60.new("-464").zero?
+    assert NewBase60.new("0").zero?
+    refute NewBase60.new("464").zero?
+  end
+
+  def test_to_s
+    assert NewBase60.new("464").to_s == "0:464"
+    assert NewBase60.new("-464").to_s == "-0:464"
+  end
+
+  def test_inspect
+    assert NewBase60.new("464").inspect == "0:464"
+    assert NewBase60.new("-464").inspect == "-0:464"
+  end
+
   def test_base60_to_base10
     assert NewBase60.new("464").to_i == 14764
     refute NewBase60.new("464").to_i == 12345
@@ -24,30 +68,40 @@ class TestCase < MiniTest::Spec
     refute NewBase60.new("464").to_date == Date.parse("2010/06/05")
   end
 
+  def test_base60_to_date_object_id
+    num = NewBase60.new("4JG")
+    assert num.to_date.object_id ==  num.to_date.object_id
+  end
+
   def test_date_to_base60
-    assert Date.parse("2010/06/04").to_sxg == NewBase60.new("464").to_s
+    assert Date.parse("2010/06/04").to_sxg == NewBase60.new("464")
     refute Date.parse("2010/06/05").to_sxg == NewBase60.new("464")
   end
 
   def test_base10_to_base60
-    assert 14764.to_sxg == "464"
-    refute 12345.to_sxg == "464"
+    assert 14764.to_sxg.to_s == "0:464"
+    refute 12345.to_sxg.to_s == "0:464"
   end
 
   def test_base10_to_base60_negative
-    assert -14764.to_sxg == "-464"
-    refute -12345.to_sxg == "-464"
+    assert (-14764).to_sxg.to_s == "-0:464"
+    refute (-12345).to_sxg.to_s == "-0:464"
   end
 
   def test_base10_to_base60_zero
-    assert 0.to_sxg == "0"
+    assert 0.to_sxg == "0:0"
   end
 
   def test_base10_to_base60_with_leading_zeroes
-    assert 14764.to_sxgf(1) == "464"
-    assert 14764.to_sxgf(5) == "00464"
-    assert 14764.to_sxgf()  == "464"
-    assert 14764.to_sxgf(9) == "000000464"
-    refute 12345.to_sxgf(9) == "000000464"
+    assert 14764.to_sxgf(1) == "0:464"
+    assert 14764.to_sxgf(5) == "0:00464"
+    assert 14764.to_sxgf()  == "0:464"
+    assert 14764.to_sxgf(9) == "0:000000464"
+    refute 12345.to_sxgf(9) == "0:000000464"
+    assert (-14764).to_sxgf(1) == "-0:464"
+    assert (-14764).to_sxgf(5) == "-0:00464"
+    assert (-14764).to_sxgf()  == "-0:464"
+    assert (-14764).to_sxgf(9) == "-0:000000464"
+    refute (-12345).to_sxgf(9) == "-0:000000464"
   end
 end
